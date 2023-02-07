f2ada68cb2bdd5dbf4b102a7af73af954ab20019
Joel Kizito <kizitojoel16@gmail.com>
2023-02-07 11:49:23 +0300

    Create file templates and basic functions
    Designed the three major files for the project and implemented the basic
    calculator functions. Add, subtract, divide and multiply.

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function add(a, b)
+{
+    return a + b;
+}
+function subtract(a, b)
+{
+    return a - b;
+}
+function multiply()
+{
+    return a * b;
+}
+function divide()
+{
+    return a / b;
+}
