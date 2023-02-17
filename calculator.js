c9dd4ed12a9be0e42868ae6a5ded3ebe614a5848
Joel Kizito <kizitojoel16@gmail.com>
2023-02-17 13:27:27 +0300

    Add responsiveness to keyboard
    Implement keyboard responsivness and live calculations

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
 const current = document.querySelector("#current");
 const previous = document.querySelector("#previous");
 
+
+
 function add(a, b)
 {
     return a + b;
@@ -34,6 +36,7 @@ function operate()
         {
             console.log(splitContent);
             screenClear();
+            previous.textContent = currentContent;
             current.textContent = add(parseFloat(splitContent[0]), parseFloat(splitContent[1]))
         }
 
@@ -46,6 +49,7 @@ function operate()
         if (splitContent.length >= 2)
         {
             screenClear();
+            previous.textContent = currentContent;
             current.textContent = subtract(parseFloat(splitContent[0]), parseFloat(splitContent[1]))
         }
     }
@@ -57,6 +61,7 @@ function operate()
         if (splitContent.length >= 2)
         {
             screenClear();
+            previous.textContent = currentContent;
             current.textContent = divide(parseFloat(splitContent[0]), parseFloat(splitContent[1]))
         }
     }
@@ -66,7 +71,9 @@ function operate()
         splitContent = currentContent.split("×");
         if (splitContent.length >= 2)
         {
+
             screenClear();
+            previous.textContent = currentContent;
             current.textContent = multiply(parseFloat(splitContent[0]), parseFloat(splitContent[1]))
         }
     }
@@ -107,18 +114,103 @@ function screenClear()
 
 
 //Add responsiveness to buttons
+
+// Array for each of the operands
+const operandSymbols = ["+", "-", "÷", "×"];
 const buttons = Array.from(document.getElementsByClassName("button"));
 buttons.forEach(button => {
     if (button.textContent !== "=")
     {
-        button.addEventListener("click", () => {current.textContent += button.textContent});
+        button.addEventListener("click", () => 
+        {
+            current.textContent += button.textContent;
+            if (operandSymbols.includes(button.textContent))
+            {
+                checkOperation();
+            }
+        }
+        );
+        
     }
 });
 
-// Handling calculations
-// const operands = Array.from(document.getElementsByClassName("operand"));
-// operands.forEach(operand => {
-//     operand.addEventListener("click", operate)
-// });
+// Handling deleting
+const deleteButton = document.querySelector(".delete");
+deleteButton.addEventListener("click", () => {
+    current.textContent = current.textContent.slice(0, -1);
+})
+
+// Keyboard responsiveness
+
+// Declare full list
+keyboardList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "(",")", ".", "-"];
+
+
+// Add event listener on keydown
+document.addEventListener('keydown', (event) => {
+    var name = event.key;
+    var code = event.code;
+    if (name === 'Shift') {
+      // Do nothing.
+      return;
+    }
+    if (event.shiftKey) {
+      console.log(`The character pressed was ${name} \n Key code Value: ${code}`);
+      if (keyboardList.includes(name))
+      {
+        current.textContent += name;
+      }
+      else if(name === "*")
+      {
+        current.textContent += "×";
+        checkOperation()
+      }
+      else if(name === "+")
+      {
+        current.textContent += name;
+        checkOperation()
+      }
+    //   console.log(mapKeyPressToActualCharacter(true, characterMap.indexOf(name)))
+
+
+    } 
+    else 
+    {
+      console.log(`Key pressed ${name} \n Key code Value: ${code}`);
+      if (keyboardList.includes(name))
+      {
+        current.textContent += name;
+      }
+      else if (name === "Enter")
+      {
+        operate();
+      }
+      else if(name === "Backspace")
+      {
+        current.textContent = current.textContent.slice(0, -1);
+      }
+      else if(name === "/")
+      {
+        current.textContent += "÷";
+      }
+    }
+  }, false);
+
+
+  // Checking for previous operand
+
+function checkOperation()
+{
+    checkContent = current.textContent;
+    // Remove the last operator
+    lastOperator = checkContent[checkContent.length - 1];
+    current.textContent = checkContent.slice(0, -1);
+
+    // Check if it has an operator already
+    operate()
+    
+    //If not, just return
+    current.textContent += lastOperator;
+}
 
 
