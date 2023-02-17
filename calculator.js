e8d91236b5e678ab8a7030298358191d4210cb1c
Joel Kizito <kizitojoel16@gmail.com>
2023-02-17 09:19:55 +0300

    Include add functionality in the operate function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,6 @@
+const current = document.querySelector("#current");
+const previous = document.querySelector("#previous");
+
 function add(a, b)
 {
     return a + b;
@@ -6,11 +9,83 @@ function subtract(a, b)
 {
     return a - b;
 }
-function multiply()
+function multiply(a, b)
 {
     return a * b;
 }
-function divide()
+function divide(a, b)
 {
     return a / b;
 }
+
+function operate()
+{
+    const currentContent = current.textContent;
+    console.log(currentContent)
+    const previousContent = previous.textContent;
+    // Specify splitting using operators, and split items using numbers
+
+    // Using add operator
+    if (currentContent.includes("+"))
+    {
+        console.log("We are here");
+        splitContent = currentContent.split("+");
+        if (splitContent.length >= 2)
+        {
+            console.log(splitContent);
+            screenClear();
+            current.textContent = add(parseFloat(splitContent[0]), parseFloat(splitContent[1]))
+        }
+
+    }
+    // splitContent = currentContent.split("");
+
+    // let a = splitContent[0]
+    // let b = splitContent[2]
+    // let operator = splitContent[1]
+    // screenClear()
+    // if(operator === "+")
+    // {
+    //     current.textContent = add(a, b)
+    // }
+    // else if (operator === "-")
+    // {
+    //     let answer = subtract(a, b)
+    // }
+    // else if (operator === "x")
+    // {
+    //     return multiply(a, b)
+    // }
+    // else if(operator === '/')
+    // {
+    //     return divide(a, b)
+    // }
+}
+
+//Create function that populates the display
+
+//Clear the screen
+function screenClear()
+{   
+    previous.textContent = "";
+    current.innerHTML = "";
+}
+
+
+
+//Add responsiveness to buttons
+const buttons = Array.from(document.getElementsByClassName("button"));
+buttons.forEach(button => {
+    if (button.textContent !== "=")
+    {
+        button.addEventListener("click", () => {current.textContent += button.textContent});
+    }
+});
+
+// Handling calculations
+// const operands = Array.from(document.getElementsByClassName("operand"));
+// operands.forEach(operand => {
+//     operand.addEventListener("click", operate)
+// });
+
+
